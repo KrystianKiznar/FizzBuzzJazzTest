c0481f9ea04b1338f12394b118ffa588587b43c5
KrystianKiznar <krystian.kiznar@gmail.com>
2022-08-24 12:58:59 +0200

    all tests passed

--- FizzBuzzJazz/FizzBuzz.cpp
+++ FizzBuzzJazz/FizzBuzz.cpp
@@ -2,10 +2,15 @@
 #include "FizzBuzz.h"
 
 std::string FizzBuzz(int input) {
-    if (input % 3 == 0)
-        return "Fizz";
-    else if (input % 5 == 0)
-        return "Buzz";
+    std::string tempo;
 
-    return std::to_string(input);
+    if (input % 3 == 0)
+        tempo += "Fizz";
+    if (input % 5 == 0)
+        tempo += "Buzz";
+    if (input % 7 == 0)
+        tempo += "Jazz";
+    if(input % 3 != 0 && input % 5 != 0 && input % 7 != 0)
+        tempo = std::to_string(input);
+    return tempo;
 }
--- FizzBuzzJazzTest/test.cpp
+++ FizzBuzzJazzTest/test.cpp
@@ -29,3 +29,60 @@ TEST(FizzBuzzJazzTest, divisible3ResultFizz) {
 TEST(FizzBuzzJazzTest, divisible5ResultFizz) {
 	checkFizzBuzz(10, "Buzz");
 }
+
+TEST(FizzBuzzJazzTest, divisible21629Result21629) {
+	checkFizzBuzz(21629, "21629");
+}
+
+TEST(FizzBuzzJazzTest, divisible21624ResultBuzz) {
+	checkFizzBuzz(21624, "Fizz");
+}
+
+TEST(FizzBuzzJazzTest, divisible21625ResultBuzz) {
+	checkFizzBuzz(21625, "Buzz");
+}
+
+TEST(FizzBuzzJazzTest, input3ResultJazz) {
+	checkFizzBuzz(7, "Jazz");
+}
+
+TEST(FizzBuzzJazzTest, input14ResultJazz) {
+	checkFizzBuzz(14, "Jazz");
+}
+
+TEST(FizzBuzzJazzTest, divisible216231ResultJazz) {
+	checkFizzBuzz(216231, "Fizz");
+}
+
+
+TEST(FizzBuzzJazzTest, divisible15ResultFizzBuzz) {
+	checkFizzBuzz(15, "FizzBuzz");
+}
+
+TEST(FizzBuzzJazzTest, divisible60ResultFizzBuzz) {
+	checkFizzBuzz(60, "FizzBuzz");
+}
+
+TEST(FizzBuzzJazzTest, divisible21ResultFizzJazz) {
+	checkFizzBuzz(21, "FizzJazz");
+}
+
+TEST(FizzBuzzJazzTest, divisible42ResultFizzJazz) {
+	checkFizzBuzz(42, "FizzJazz");
+}
+
+TEST(FizzBuzzJazzTest, divisible35ResultBuzzJazz) {
+	checkFizzBuzz(35, "BuzzJazz");
+}
+
+TEST(FizzBuzzJazzTest, divisible140ResultBuzzJazz) {
+	checkFizzBuzz(140, "BuzzJazz");
+}
+
+TEST(FizzBuzzJazzTest, divisible140ResultFizzBuzzJazz) {
+	checkFizzBuzz(105, "FizzBuzzJazz");
+}
+
+TEST(FizzBuzzJazzTest, divisible840ResultFizzBuzzJazz) {
+	checkFizzBuzz(840, "FizzBuzzJazz");
+}
